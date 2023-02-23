af3e1a8ad22a5d2c00f81ce0f893cc03e2aca3d0
Enzo Ruettimann <enzoruettimann@icloud.com>
2023-02-23 19:46:59 +0100

    Finished with displaying all team member tiles

--- template-leitungsteam.php
+++ template-leitungsteam.php
@@ -16,14 +16,15 @@ Template Name: Leitungsteam
             while (have_rows('scoutleader_leitungsperson')) :
                 the_row();
         ?>
-                <div class="scoutleader-container col-md-3">
+                <div class="scoutleader-container col-xl-3 col-lg-4 col-md-6">
                     <div class="scoutleader-img-container">
                         <?php
                         $imgUrl = get_sub_field('scoutleader_bild')['url'];
                         $forename = get_sub_field('scoutleader_vorname');
                         $surname = get_sub_field('scoutleader_nachname');
                         $vulgo = get_sub_field('scoutleader_vulgo');
-                        $job = get_sub_field('scoutleader_funktionsname');
+                        $job = get_sub_field('scoutleader_funktion1');
+                        $job2 = get_sub_field('scoutleader_funktion2');
                         ?>
                         <img src="<?php echo $imgUrl; ?>" alt="<?php echo $forename . ' ' . $surname . ' vo ' . $vulgo; ?>" />
 
@@ -33,12 +34,13 @@ Template Name: Leitungsteam
                         <div class="scoutleader-info-txt">
                             <h5><?php echo $forename . ' ' . $surname; ?></h5>
                             <h5><?php echo ' v/o ' . $vulgo; ?></h5>
-                            <h6><?php echo $job; ?></h6>
+                            <h6><?php echo $job; ?>  <br/></h6>
+                            <h6><?php echo $job2; ?>  <br/></h6>
                         </div>
-                        <div class="scoutleader-mail">
+                        <a class="scoutleader-mail" href="mailto:<?php echo $vulgo; ?>@pfadihelveter.ch"><div>
                             E-Mail <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-envelope" viewBox="0 0 16 16">
                                 <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
-                            </svg></div>
+                            </svg></div></a>
                     </div>
                 </div>
         <?php
