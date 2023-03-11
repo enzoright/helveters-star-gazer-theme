caffce9c071c703eea803e97b8967744d1cea948
Enzo Ruettimann <enzoruettimann@icloud.com>
2023-03-11 12:29:30 +0100

    Style Changes #1

--- page.php
+++ page.php
@@ -1,6 +1,6 @@
 <?php get_header();?>
 
-<section class="page-wrap">
+<section class="page-wrap normal-page">
 <div class="container content-container">
     <h1><?php the_title();?></h1>
     <?php get_template_part('includes/section', 'content');?>
